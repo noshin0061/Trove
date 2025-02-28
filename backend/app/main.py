556eca09c23374cc446d4c244356251be90924d7
noshin0061 <noshin0408@icloud.com>
2025-03-01 02:57:52 +0900

    prepare backend deploy

--- backend/app/main.py
+++ backend/app/main.py
@@ -18,7 +18,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "http://localhost:3000",
-        "https://trove-virid.vercel.app/"  # フロントエンドのドメインを追加
+        "https://trove-virid.vercel.app"  # フロントエンドのドメインを追加
     ],
     allow_credentials=True,
     allow_methods=["*"],
